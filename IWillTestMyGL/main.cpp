f5b33814cadb63a4616a1d221d6b3bfbbe22df14
LX.. <1659404657@qq.com>
2024-10-27 00:01:05 +0800

    添加着色器代码并且成功绘制内容

--- IWillTestMyGL/main.cpp
+++ IWillTestMyGL/main.cpp
@@ -1,20 +1,41 @@
 #include "GLWindow.h"
-#include "GLVertexShader.h"
-#include "GLFragmentShader.h"
+#include "GLShader.h"
+#include "GLMesh.h"
 
 
+GLShader* shader;
+GLMesh* mesh;
 void OnInit()
 {
+	shader = GLShader::Load("VertexShader.vsh", "FragmentShader.psh");
+
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f, 1.0f,  0.0f, 0.0f,
+		-0.5f,  0.5f, 0.0f,  0.0f, 1.0f, 1.0f,  0.0f, 0.0f, 1.0f,  0.0f, 1.0f,
+		 0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f,  0.0f, 0.0f, 1.0f,  1.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 1.0f,  0.0f, 0.0f, 1.0f,  1.0f, 0.0f
+	};
+	unsigned int indices[] = {
+		// 注意索引从0开始! 
+		// 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+		// 这样可以由下标代表顶点组合成矩形
+
+		0, 1, 2,
+		0, 2, 3
+	};
+	mesh = new GLMesh(vertices, sizeof(vertices) / sizeof(vertices[0]), indices, sizeof(indices) / sizeof(indices[0]));
 }
 
 void OnRendering()
 {
-
+	shader->Use();
+	mesh->Flush();
 }
 
 void OnClosing()
 {
-
+	delete shader;
+	delete mesh;
 }
 
 int main()
