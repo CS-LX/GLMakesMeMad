e26ed934eb12c8215d9bfee40185b775d8feede0
LX.. <1659404657@qq.com>
2024-10-29 19:45:41 +0800

    修bug，C#端成功绘制图元

--- CLITestsMyGL/GLSMesh.cpp
+++ CLITestsMyGL/GLSMesh.cpp
@@ -4,11 +4,11 @@ GLSMesh::GLSMesh(array<float>^ vertices, array<unsigned int>^ indices)
 {
 	std::vector<float> verticesRaw;
 	std::vector<unsigned int> indicesRaw;
-	for each (int vertex in vertices)
+	for each (float vertex in vertices)
 	{
 		verticesRaw.push_back(vertex);
 	}
-	for each (int index in indices)
+	for each (unsigned int index in indices)
 	{
 		indicesRaw.push_back(index);
 	}
--- IWillTestMyGL/main.cpp
+++ IWillTestMyGL/main.cpp
@@ -11,11 +11,18 @@ GLMesh* mesh;
 void OnInit()
 {
 	shader = GLShader::Load("VertexShader.vsh", "FragmentShader.psh");
-	std::vector<GLVertex> vertices;
-	vertices.push_back(GLVertex(Vector3(-0.5f, -0.5f, 0.0f), Color(0.0f, 0.0f, 1.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(0.0f, 0.0f)));
-	vertices.push_back(GLVertex(Vector3(-0.5f, 0.5f, 0.0f), Color(0.0f, 1.0f, 1.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(0.0f, 1.0f)));
-	vertices.push_back(GLVertex(Vector3(0.5f, 0.5f, 0.0f), Color(1.0f, 1.0f, 1.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(1.0f, 1.0f)));
-	vertices.push_back(GLVertex(Vector3(0.5f, -0.5f, 0.0f), Color(1.0f, 0.0f, 1.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(1.0f, 0.0f)));
+	//std::vector<GLVertex> vertices;
+	//vertices.push_back(GLVertex(Vector3(-0.5f, -0.5f, 0.0f), Color(0.0f, 0.0f, 1.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(0.0f, 0.0f)));
+	//vertices.push_back(GLVertex(Vector3(-0.5f, 0.5f, 0.0f), Color(0.0f, 1.0f, 1.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(0.0f, 1.0f)));
+	//vertices.push_back(GLVertex(Vector3(0.5f, 0.5f, 0.0f), Color(1.0f, 1.0f, 1.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(1.0f, 1.0f)));
+	//vertices.push_back(GLVertex(Vector3(0.5f, -0.5f, 0.0f), Color(1.0f, 0.0f, 1.0f), Vector3(0.0f, 0.0f, 1.0f), Vector2(1.0f, 0.0f)));
+
+	std::vector<float> vertices = {
+		-0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+			-0.5f, 0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f,
+			0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+			0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f
+	};
 
 	std::vector<unsigned int> indices = {0, 1, 2, 0, 2, 3};
 
